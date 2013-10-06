cb0f2e43d5373f95494218e809d05384ec05e0d3
Victor Mak <victormakcy@gmail.com>
2013-10-06 13:47:10 -0400

    initial commit

---
+++ scripts/scripts.js
@@ -0,0 +1,11 @@
+/* quote-face scripts
+   Author: Victor Mak
+*/
+
+
+$(document).ready(function(){
+    var browserwidth = $(window).width();
+    var browserheight = $(window).height();
+
+    $('.section').height(browserheight);
+});
